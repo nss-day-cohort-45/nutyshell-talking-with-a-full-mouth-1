09e0e1c721e974a9e16032be0b0c8851c81af662
Gregory Thompson <mrgregorythompsonii@gmail.com>
2021-01-13 11:27:29 -0600

    set up login page; added homepage.css for future styling

--- src/scripts/Nutshell.js
+++ src/scripts/Nutshell.js
@@ -1,3 +1,5 @@
 export const Nutshell = () => {
     // Render all your UI components here
+    console.log("Nutshell has been rendered!")
+    console.log("Session storage user id: ", sessionStorage.getItem("activeUser") )
 }

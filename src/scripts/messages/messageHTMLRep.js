31bf670ed2b5d56d7ab4a4946c3c08355dd1c0b2
Xine75 <cjdoza@gmail.com>
2021-01-13 13:12:15 -0600

    working on PR

--- src/scripts/messages/messageHTMLRep.js
+++ src/scripts/messages/messageHTMLRep.js
@@ -5,14 +5,17 @@
 export const messageHTMLRep = (messageObj, users) => {
     return `
     <section class="message" id="message">
-        <div class="message__author" id="message__author">${ messageObj.}
-        </div>
-        <div class="message__text" id="message__text"> 
+        <div class="message__author" id="message__author">
         ${ 
             users.map(user => `${ user.username }`)
-         }
+        }
+        </div>
+        <div class="message__text" id="message__text"> 
+        ${ messageObj.text}
          </div>
 
     </section>
     `
 }
+
+//Still need to figure out how to match user id's to find username
---
+++ src/scripts/messages/messageList.js
@@ -0,0 +1,10 @@
+//Christine
+
+//imports useMessages, getMessages, and saveMessages from provider
+//imports HTMLRep
+//renders messageList to the DOM
+//communicates with provider when sendMessage btn is clicked
+
+import { useMessages, getMessages, saveMessages } from "./messageProvider.js"
+import { messageHTMLRep } from "./messageProvider.js"
+
